887f80509baeac3422987f2324352543f5e9e46b
VHRE03 <vh.romero.e@gmail.com>
2026-09-22 21:32:17 -0600

    fix: Correccion de BaseProjectAutoConfiguration

--- src/main/java/com/vhre/base/config/BaseProjectAutoConfiguration.java
+++ src/main/java/com/vhre/base/config/BaseProjectAutoConfiguration.java
@@ -1,11 +1,9 @@
 package com.vhre.base.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-@Configuration
+@AutoConfiguration
 @Import(JacksonConfig.class)
-@ComponentScan(basePackages = "com.vhre.base")
 public class BaseProjectAutoConfiguration {
 }
